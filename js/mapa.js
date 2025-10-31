b68e3eb93ecfe49e6353760508486ee846bfbae6
santihpuig <santihpuig@gmail.com>
2025-10-31 14:00:29 +0100

    centro mapa

--- js/mapa.js
+++ js/mapa.js
@@ -9,7 +9,7 @@ var map = L.map("map", {
   attributionControl: true,
   fullscreenControl: true,
   //maxBounds: bounds,
-}).setView([39.39006746517305, -0.39562319764050224], 14);
+}).setView([39.400412463222494, -0.39934490710677306], 15);
 
 var simple = L.tileLayer(
   "https://api.mapbox.com/styles/v1/santihpuig/cmf0z178x01k701qw157p0wb6/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FudGlocHVpZyIsImEiOiJrYkhOMDVnIn0.ak6qwXtkOps01I5G-LCS_A",
